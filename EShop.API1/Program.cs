6d085e328d1594d17cc8b2d2cf1fc6466e131c75
uyara <uyara@DESKTOP-7K1F8KE>
2023-04-10 18:59:05 +0300

    Add project files.

---
+++ EShop.API1/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opts =>
+    {
+        opts.Authority = "https://localhost:7294";
+        opts.Audience = "resource_api1";
+    });
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ EShop.AuthServer/Config.cs
@@ -0,0 +1,68 @@
+﻿using IdentityServer4.Models;
+
+namespace EShop.AuthServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("resource_api1")
+                {
+                    Scopes = {
+                        "api1.read",
+                        "api1.write",
+                        "api1.update",
+                    }
+                },
+                new ApiResource("resource_api2")
+                {
+                    Scopes = {
+                        "api2.read",
+                        "api2.write",
+                        "api2.update",
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new List<ApiScope>()
+            {
+                new ApiScope("api1.read","read permission for api1"),
+                new ApiScope("api1.write","write permission for api1"),
+                new ApiScope("api1.update","update permission for api1"),
+                new ApiScope("api2.read","read permission for api2"),
+                new ApiScope("api2.write","write permission for api2"),
+                new ApiScope("api2.update","update permission for api2"),
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId = "Client1",
+                    ClientName = "Client 1 Web App",
+                    ClientSecrets = new [] {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"api1.read"}
+                },
+                new Client()
+                {
+                    ClientId = "Client2",
+                    ClientName = "Client 2 Web App",
+                    ClientSecrets = new [] {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = {"api1.read","api2.write","api2.update"}
+                }
+            };
+        }
+
+
+    }
+}
